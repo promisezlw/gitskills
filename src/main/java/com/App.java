7b9c4294b0e22b3eefd22d6978985f2452f4e3ec
promise <promisezlw@163.com>
2020-03-20 20:44:14 +0800

    代码1.0

---
+++ src/main/java/com/App.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
+//java -jar clouddemo-0.0.1-SNAPSHOT.jar --server.port=8889
